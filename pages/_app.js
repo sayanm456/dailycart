fc9141629325eb9be0389422c91350aea7f8831f
sayanm456 <89853045+sayanm456@users.noreply.github.com>
2022-10-03 22:14:41 +0530

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,77 @@
-import '../styles/globals.css'
+import { useState , useEffect } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [cart, setCart] = useState({})
+  const [subTotal, setSubTotal] = useState(0)
+  useEffect(() => {
+     console.log("Hey, I am useEffect from _app.js")
+     try {
+        if(localStorage.getItem("cart")){
+          setCart(JSON.parse(localStorage.getItem("cart")))
+        }
+      
+     } catch (error) {
+        console.error(error);
+        localStorage.clear()
+     }
+
+     
+  }, [])
+  
+
+
+  const saveCart = (myCart)=>{
+     localStorage.setItem("cart", myCart)
+     let subt = 0;
+     let keys = Object.keys(cart)
+     for(let i=0; keys.length; i++ ){
+        subt += myCart[keys[i]].price * myCart[keys[i]].qty
+     }
+     setSubTotal(subt)
+  }
+  const addToCart = (itemCode, qty, price, name, size, variant)=>{
+     let newCart = cart;
+     if(itemCode in cart){
+        newCart[itemCode].qty = cart[itemCode].qty + qty
+     }
+     else{
+        newCart[itemCode] = { qty: 1, price, name, size, variant}
+     }
+     setCart(newCart)
+     saveCart(newCart)
+  }
+
+  const clearCart = ()=>{
+     setCart({})
+     saveCart({})
+  }
+
+  const removeFromCart = (itemCode, qty, price, name, size, variant)=>{
+    let newCart = cart;
+    if(itemCode in cart){
+       newCart[itemCode].qty = cart[itemCode].qty - qty
+    }
+    if(newCart[itemCode]["qty"]<=0){
+       delete newCart[itemCode]
+    }
+
+    setCart(newCart)
+    saveCart(newCart)
+ }
+
+
+
+  return (
+    <>
+      <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart}  subTotal={subTotal}/>
+      <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} {...pageProps} subTotal={subTotal}/>
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
