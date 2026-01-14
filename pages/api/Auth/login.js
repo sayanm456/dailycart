147ea12b0e345b25be3506c172844ea2db404f03
Sayan <sayanm2345@gmail.com>
2026-01-15 00:01:46 +0530

    file updatation

--- pages/api/Auth/login.js
+++ pages/api/Auth/login.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import User from "../../models/User"
-import connectDb from "../../middleware/db"
+import User from "../../../models/User"
+import connectDb from "../../../middleware/db"
 var CryptoJS = require("crypto-js");
 var jwt = require('jsonwebtoken');
 
